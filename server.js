f595ebf832110fa3afbacdeb70bfb9ea2cac6b38
Andrew Rasmussen <andrewras93@gmail.com>
2020-12-04 14:32:31 +0100

    Tilføjet velkomst besked til nye brugere samt. besked til øvrige brugere, at en ny bruger har tilkoblet sig chatten, og en besked når en bruger forlader chatten

--- server.js
+++ server.js
@@ -7,10 +7,24 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// Sætter den til at køre vores index side, når serveren er i gang
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Kører når en bruger opretter forbindelse
 io.on('connection', socket => {
-    console.log('New WS Connection..')
+
+    // Velkomst besked til den nye bruger
+    socket.emit('message', 'Velkommen til meeton.');
+
+    // Gør opmærksom på en ny bruger har tilkoblet sig chatten
+    socket.broadcast.emit('message', 'En bruger har tilkoblet sig chatten.');
+
+    // Kører når en bruger forlader chatten
+    socket.on('disconnect', () => {
+        io.emit('message', 'En bruger har forladt chatten.');
+    });
+
+
 });
 
 const PORT = 3000 || process.env.PORT;
